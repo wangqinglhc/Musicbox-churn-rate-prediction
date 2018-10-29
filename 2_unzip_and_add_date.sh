473d2a00bc4e7ca2b65ddc5a21bee9ee4073fb32
wangqinglhc <wangqinglhc0122@ou.edu>
2018-10-28 19:04:56 -0500

    uploading scripts and notebooks

--- 2_unzip_and_add_date.sh
+++ 2_unzip_and_add_date.sh
@@ -1,29 +1,29 @@
-## working dir is src
+# working dir is src
 
-#### process play log ####
-# unzip play log
-# cd data/raw/
-# for f in *_play.log.tar.gz
-# do
-#  echo "Processing $f"
-#  tar -xvzf $f 
-#  #rm $f
-# done
+### process play log ####
+unzip play log
+cd data/raw/
+for f in *_play.log.tar.gz
+do
+ echo "Processing $f"
+ tar -xvzf $f 
+ #rm $f
+done
 
-# mv *_play.log ../play/
+mv *_play.log ../play/
 
-# # append file_name to each row (will be used for date)
-# cd ../play/
-# for f in *.log
-# do
-#  echo "Processing $f"
-#  awk -v var="$f" '{print $0,"\t",substr(var,1,8)}' $f > ${f}.fn
-# done
+# append file_name to each row (will be used for date)
+cd ../play/
+for f in *.log
+do
+ echo "Processing $f"
+ awk -v var="$f" '{print $0,"\t",substr(var,1,8)}' $f > ${f}.fn
+done
 
-# # cat all log with filename to one file
-# cat *.log.fn > all_play_log
-# rm *.log
-# rm *.log.fn
+# cat all log with filename to one file
+cat *.log.fn > all_play_log
+rm *.log
+rm *.log.fn
 
 
 
